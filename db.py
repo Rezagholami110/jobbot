5702f86603d0c99181cbfb9ab0d0df50fbc1cdf9
Rezagholami110 <110648786+Rezagholami110@users.noreply.github.com>
2026-02-24 14:16:14 +0400

    Create db.py

---
+++ db.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+conn = sqlite3.connect("bot.db", check_same_thread=False)
+
+conn.execute("""
+CREATE TABLE IF NOT EXISTS keywords (
+    user_id INTEGER,
+    keyword TEXT,
+    PRIMARY KEY(user_id, keyword)
+)
+""")
+
+conn.commit()
+
+def add_keyword(user_id, keyword):
+    conn.execute("INSERT OR IGNORE INTO keywords VALUES (?, ?)", (user_id, keyword))
+    conn.commit()
+
+def list_keywords(user_id):
+    cursor = conn.execute("SELECT keyword FROM keywords WHERE user_id=?", (user_id,))
+    return [row[0] for row in cursor.fetchall()]
